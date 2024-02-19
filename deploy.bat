b87cd4b6236c224b33e98e9e9a3695351a6b4e70
amooseinaroom <amooseinaroom@users.noreply.github.com>
2024-02-19 12:35:02 +0100

    added deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set destination=deploy\
+
+xcopy /Y assets\fonts\stanberry\Stanberry.ttf %destination%assets\fonts\stanberry\Stanberry.ttf
+xcopy /Y customization\character_template.png %destination%customization\character_template.png
+xcopy /Y chatworld.exe %destination%chatworld.exe
